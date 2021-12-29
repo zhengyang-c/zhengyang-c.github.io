bed2f9e49d6bce3c9fb4215583ade8aceec29673
Zheng Yang <zchoong001@e.ntu.edu.sg>
2021-12-29 11:52:43 +0800

    Test with image

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+document.addEventListener("DOMContentLoaded",function(){
+  console.log("Hello!")
+  var a = document.querySelector('.blur-image');
+  if (!a){
+	  console.log("failed");
+  }
+
+  a.style.backgroundImage = "url(assets/splash.jpg)";
+
+})
